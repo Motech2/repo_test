95277cb619d72fc2730669cbd17e5a391e316976
Motech2 <Mohamad.thrn7@gmail.com>
2020-06-16 01:47:27 +0430

    'First_complete_view'

--- Library/catalog/views.py
+++ Library/catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+
+def index(request):
+    """
+    View function for home page
+    """
+    # generates count of the some of main object
+    num_books = models.Book.objects.all().count()
+    num_instances = models.BookInstance.objects.all().count()
+    # Available books
+    avl_instances = models.BookInstance.objects.filter(status__exact='a').count()
+    num_authors = models.Author.objects.count()  # the 'all()' is implied by default
+
+    # Render the 'index.html' with the data in context
+    return render(request,
+                  'catalog/index.html',
+                  context={'num_books': num_books, 'num_instances': num_instances, 'avl_instances': avl_instances,
+                           'num_authors': num_authors})
+
