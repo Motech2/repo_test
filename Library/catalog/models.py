5d931c810e67802cd727e568c386ebca47e3f133
Motech2 <Mohamad.thrn7@gmail.com>
2020-06-15 23:35:38 +0430

    '2020-06-15_23-35'

--- Library/catalog/models.py
+++ Library/catalog/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
-# Create your models here.
+class Book(models.Model):
+    """
+    Model representing a book (but not a specific copy of a book).
+    """
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True)
+    genre = models.ManyToManyField('Genre', help_text='Select a genre for this book')
+    title = models.CharField(max_length=200)
+    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
+    isbn = models.CharField('ISBN', max_length=13,
+                            help_text='13 character <a href="https://isbn-international.org">ISBN</a>')
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.title
+
+    def get_absolute_url(self):
+        """
+        returns The url to access a particular author instance.
+        """
+        return reverse('author-detail', args=[str(self.id)])
+
+
+class BookInstance(models.Model):
+    """
+    Model representing a specific copy of a book (i.e , that can be borrowed from the library)
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book')
+    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+
+    Loan_status = (
+        ('m', 'Maintenance'),
+        ('o', 'on loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved')
+    )
+    status = models.CharField(max_length=1, choices=Loan_status, blank=True, default='m', help_text='Book Availability')
+
+
+class Genre(models.Model):
+    """
+    Model representing a book genre
+    """
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction, French Poetry etc)')
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.name
